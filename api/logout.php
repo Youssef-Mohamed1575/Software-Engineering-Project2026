c613bd8b09f8a93a72e604c4871d9e32ff041238
youssef_tag <yousseftag50@gmail.com>
2026-05-08 18:59:50 +0300

    undo of the new logout api

--- api/logout.php
+++ api/logout.php
@@ -1,33 +1,11 @@
 <?php
 session_start();
-
-header('Content-Type: application/json');
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-
-$_SESSION = [];
 session_unset();
-
-// Delete session cookie
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        '/',
-        '',
-        false,
-        true
-    );
-}
-
-// Force destroy session file
 session_destroy();
+header('Content-Type: application/json');
 
 echo json_encode([
     'success' => true,
     'message' => 'Logged out successfully'
 ]);
-exit;
 ?>
